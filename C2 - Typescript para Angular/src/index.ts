39eed426214f1a165ccd223de866453a297732eb
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2024-01-03 15:24:09 -0300

    Update index.ts

--- C2 - Typescript para Angular/src/index.ts
+++ C2 - Typescript para Angular/src/index.ts
@@ -205,9 +205,52 @@ class funcionario { }
 class Quincas{}
 
 */
+ 
 
 function apiVersion(version: string) {
-    return (target) => {
-        Object.assign(target.prototype, {__version: version});
-};
+    return (target: any) => {
+        Object.assign(target.prototype , { __version: version, __name:"Andre" });
+    }
 }
+/*
+
+@apiVersion("1.10")
+class Api { }
+
+const api = new Api()
+console.log(api.__name);*/
+ /*
+//sttribute decoratyor
+
+function minLength(length: number) {
+    return (target: any, key: string) => {
+        let _value = target[key];
+//pode concatenar texto, numero... a funcão
+        const getteer = () => "[play] " + _value;
+        const setter = (value: string) => {
+            if (value.length < length) {
+                throw new Error(`Tamanho menos que ${length}`);
+            } else {
+                _value = value;
+            }
+        };
+        Object.defineProperty(target, key, {
+            get: getteer,
+            set: setter,
+        });
+    };
+    
+}
+
+class Api{
+    @minLength(3)
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+const api = new Api("usa");
+console.log(api.name);
+
+    */
+   
